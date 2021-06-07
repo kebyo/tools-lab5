a9e4a64f8042ef32e81135ec526557df2b726c88
Oybek <oybekmuslimov15@gmail.com>
2021-06-07 22:52:43 +0300

    create workspace controller, route, service

---
+++ src/routes/workspace.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import WorkspacesController from '../controllers/workspace.js';
+
+const router = express.Router();
+
+router.get('/:id', WorkspacesController.get);
+
+router.post('/', WorkspacesController.create);
+
+router.patch('/:id', WorkspacesController.update);
+
+router.delete('/:id', WorkspacesController.remove);
+
+export default router;
---
+++ src/services/workspace.js
@@ -0,0 +1,25 @@
+import Workspace from '../database/models/workspace.js';
+
+export default class WorkspaceService {
+    static async create(workspace) {
+        const ws = new Workspace(workspace);
+
+        return ws.save();
+    }
+
+    static async findById(id) {
+        return Workspace.findById(id);
+    }
+
+    static async remove(id) {
+        return Workspace.findOneAndDelete({_id: id});
+    }
+
+    static async update(id, updatedWorkspace) {
+        return Workspace.findOneAndUpdate({_id: id}, {
+            $set: updatedWorkspace,
+        }, {
+            new: true,
+        });
+    }
+}
